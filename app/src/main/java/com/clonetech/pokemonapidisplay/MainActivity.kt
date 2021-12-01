27cacca7176854874fa96dd186978dc5f67170b1
scott <scottajevans@gmail.com>
2021-12-01 22:47:03 +0000

    Adding ID into display and updating on UI thread

--- app/src/main/java/com/clonetech/pokemonapidisplay/MainActivity.kt
+++ app/src/main/java/com/clonetech/pokemonapidisplay/MainActivity.kt
@@ -7,24 +7,27 @@ import android.widget.TextView
 import okhttp3.*
 import okio.IOException
 import org.json.JSONObject
+import kotlin.properties.Delegates
 
 private val client = OkHttpClient()
-private var pokemon : String? = null
 private var id : Int = 1
 
-private var nameTextView : TextView? = null
-
-
 class MainActivity : AppCompatActivity() {
+    private lateinit var nameTextView : TextView
+    private lateinit var idTextView : TextView
+    private lateinit var nextButton: Button
+    private lateinit var prevButton: Button
 
-    lateinit var nextButton: Button
-    lateinit var prevButton: Button
+    private var pokemon by Delegates.observable(JSONObject()) { property, oldValue, newValue ->
+        updatePokemonInfo(newValue)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         makeAPICall(id)
-        nameTextView = findViewById<TextView>(R.id.name)
+        nameTextView = findViewById(R.id.name)
+        idTextView = findViewById(R.id.id)
         nextButton = findViewById(R.id.next)
         prevButton = findViewById(R.id.previous)
 
@@ -38,30 +41,32 @@ class MainActivity : AppCompatActivity() {
             makeAPICall(id)
         }
     }
-}
 
-private fun updateName (name : String) {
-    nameTextView!!.text = name
-}
-
-private fun makeAPICall(id : Int) {
-    val request = Request.Builder()
-        .url("https://pokeapi.co/api/v2/pokemon/$id")
-        .build()
-
-    client.newCall(request).enqueue(object : Callback {
-        override fun onFailure(call: Call, e: IOException) {
-            e.printStackTrace()
+    private fun updatePokemonInfo(pokemon: JSONObject) {
+        this@MainActivity.runOnUiThread {
+            nameTextView.text = pokemon.get("name").toString()
+            idTextView.text = id.toString()
         }
+    }
 
-        override fun onResponse(call: Call, response: Response) {
-            response.use {
-                if (!response.isSuccessful) throw IOException("Unexpected code $response")
+    private fun makeAPICall(id : Int) {
+        val request = Request.Builder()
+            .url("https://pokeapi.co/api/v2/pokemon/$id")
+            .build()
 
-                var jObject: JSONObject = JSONObject(response.body!!.string())
-                pokemon = jObject.get("name").toString()
-                updateName(pokemon!!)
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                e.printStackTrace()
             }
-        }
-    })
+
+            override fun onResponse(call: Call, response: Response) {
+                response.use {
+                    if (!response.isSuccessful) throw IOException("Unexpected code $response")
+
+                    val jObject = JSONObject(response.body!!.string())
+                    pokemon = jObject
+                }
+            }
+        })
+    }
 }
